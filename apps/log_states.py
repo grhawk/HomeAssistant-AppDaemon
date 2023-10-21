ab8108e832bb4888e91ff90c0618c8d98b0b9583
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2023-10-21 18:56:13 +0200

    :sparkles: app to log the states of an entity

---
+++ apps/log_states.py
@@ -0,0 +1,29 @@
+from typing import Coroutine
+
+import appdaemon.adapi
+import hassapi as hass
+
+
+class LogStates(hass.Hass, appdaemon.adapi.ADAPI):
+    handle: Coroutine
+
+    def log(self, msg: str, level="INFO", **kwargs) -> None:
+        super().log(__class__.__name__ + ": " + msg, level=level, **kwargs)
+
+    def initialize(self):
+        self.log("LogStates initialized")
+        self.handle = self.listen_state(self.log_state, self.args["entity_id"])
+
+    def log_state(self, entity, attribute, old, new, kwargs):
+        self.log("---------------------------------------------")
+        self.log(">>>>>>>>>>>>>State changed<<<<<<<<<<<<<<<<<<<")
+        self.log("Entity: " + entity)
+        self.log("old #" + old + "#")
+        self.log("new #" + new + "#")
+        self.log("kwargs: " + str(kwargs))
+        self.log("attribute: " + str(attribute))
+        self.log(">>>>>>>>>>>State changed end<<<<<<<<<<<<<<<<<")
+        self.log("---------------------------------------------")
+
+
+
