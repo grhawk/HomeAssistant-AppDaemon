48c7d43678faaf1ac56bc5efebcadd119ff769bd
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2025-01-28 23:24:45 +0100

    :sparkles: rotary toggle lights when hold

--- apps/rotary_smart_switch.py
+++ apps/rotary_smart_switch.py
@@ -10,6 +10,10 @@ class RotarySmartSwitch(hass.Hass, appdaemon.adapi.ADAPI):
     def initialize(self):
         for rotary_switch in self.args["rotary_switches"]:
             self.handles_toggle.append(self.listen_state(self.toggle_callback, rotary_switch["action"], new="toggle"))
+            self.handles_toggle.append(self.listen_state(self.toggle_callback_hold, rotary_switch["action"], new="hue_move"))
 
     def toggle_callback(self, entity, attribute, old, new, kwargs):
         self.toggle(self.args["light"])
+
+    def toggle_callback_hold(self,entity, attribute, old, new, kwargs):
+        self.toggle(self.args["light_hold"])
